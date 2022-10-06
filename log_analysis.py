5c8d4089ca9aff8b7254dc0f4d433185ba7eccc8
monadnadnad <hmcust810114514@yahoo.co.jp>
2022-10-06 13:16:50 +0900

    logをparametrizeして分析の設定のみ状態として持つように変更

--- log_analysis.py
+++ log_analysis.py
@@ -10,17 +10,16 @@ class LogAnalyzer:
     """
     ログから情報を構築する
     """
-    def __init__(self, log: Log):
+    def __init__(self):
         # 待ち椅子含めている可能性のある最大の人数
         self.MAX_PLAYER_NUM = 6
         # この時間だけは状態が変わらなくても入店していると判断する
         self.MAX_STABLE_TIME = datetime.timedelta(minutes=30)
         # 再入店までに最低でもかかる時間（再入店と誤認しないための精度上げに使う）
         self.MAX_TIME_BETWEEN_REENTRY = datetime.timedelta(hours=1)
-        self.log = log
-    def _compressed_positions(self, p: Player):
+    def _compressed_positions(self, log: Log, p: Player):
         # plotするとき扱いやすいよう[0, -MAX_PLAYER_NUM]に圧縮する
-        positions = self.log.get_player_positions(p)
+        positions = log.get_player_positions(p)
         for i in range(len(positions)):
             pos = positions[i]
             if pos == None:
@@ -30,15 +29,15 @@ class LogAnalyzer:
             else:
                 positions[i] = -pos
         return positions
-    def get_status_changed(self, p: Player) -> list[bool]:
+    def get_status_changed(self, log: Log, p: Player) -> list[bool]:
         """
         プレイした事が確定したタイミングを記録したリストを返す
         """
-        times = self.log.get_times()
-        positions = self.log.get_player_positions(p)
+        times = log.get_times()
+        positions = log.get_player_positions(p)
         N = len(positions)
         ret = [False]*N
-        all_log = [self.log.get_log_at_time(times[i]) for i in range(N)]
+        all_log = [log.get_log_at_time(times[i]) for i in range(N)]
         for i in range(N-1):
             p1 = positions[i]
             p2 = positions[i+1]
@@ -73,12 +72,12 @@ class LogAnalyzer:
             #        ret[i] = True
             #        break
         return ret
-    def get_player_in_venue(self, p: Player) -> list[bool]:
+    def get_player_in_venue(self, log: Log, p: Player) -> list[bool]:
         """
         プレイヤーがその時間にいた可能性をboolで表現したリスト
         """
         # 入店の確定位置を探す、最後の確定位置以降は退店しているとみなせる
-        status_changed = self.get_status_changed(p)
+        status_changed = self.get_status_changed(log, p)
         N = len(status_changed)
         ret = [False]*N
         # 推定退店時間のindex
@@ -92,7 +91,7 @@ class LogAnalyzer:
             return ret
         # 尺取り法で入店確定位置から閾値時間だけ入店していると判定する
         l, r = 0, 1
-        times = self.log.get_times()
+        times = log.get_times()
         while r <= N:
             if status_changed[l]:
                 while r < N and times[r-1] - times[l] <= self.MAX_STABLE_TIME:
@@ -122,43 +121,43 @@ class LogAnalyzer:
         for i in range(last + 1, N):
             ret[i] = False
         return ret
-    def get_players_over_time(self) -> list[set[Player]]:
+    def get_players_over_time(self, log: Log) -> list[set[Player]]:
         """
         各時間にいた可能性のあるプレイヤーの集合のリスト
         """
-        N = len(self.log.get_times())
+        N = len(log.get_times())
         ret = [set() for _ in range(N)]
-        ps = self.log.get_players()
+        ps = log.get_players()
         player_in_venue = {}
         for p in ps:
-            player_in_venue[p] = self.get_player_in_venue(p)
+            player_in_venue[p] = self.get_player_in_venue(log, p)
         for i in range(N):
             for p in ps:
                 if player_in_venue[p][i]:
                     ret[i].add(p)
         return ret
-    def plot_player_in_venue(self, p: Player, show=True):
-        data = self.get_player_in_venue(p)
+    def plot_player_in_venue(self, log: Log, p: Player, show=True):
+        data = self.get_player_in_venue(log, p)
         #data = list(map(lambda x: -self.MAX_PLAYER_NUM*(not x), data))
         plt.plot(data, label="in venue")
         plt.legend()
         if show:
             plt.show()
-    def plot_status_changed(self, p: Player, show=True):
-        data = self.get_status_changed(p)
+    def plot_status_changed(self, log: Log, p: Player, show=True):
+        data = self.get_status_changed(log, p)
         plt.plot(data, label="status changed")
         plt.legend()
         if show:
             plt.show()
-    def plot_positions(self, p: Player, show=True):
-        positions = self._compressed_positions(p)
+    def plot_positions(self, log: Log, p: Player, show=True):
+        positions = self._compressed_positions(log, p)
         plt.plot(positions, label="position")
         plt.legend()
         if show:
             plt.show()
-    def plot_headcounts(self):
-        times = self.log.get_times()
-        playersets = self.get_players_over_time()
+    def plot_headcounts(self, log: Log):
+        times = log.get_times()
+        playersets = self.get_players_over_time(log)
         headcounts = [len(ps) for ps in playersets]
         fig, ax = plt.subplots(figsize=(8,4))
         ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
