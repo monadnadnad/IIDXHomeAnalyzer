5f16b8fdc936f8f2ae346373679f4e83e2940c97
monadnadnad <hmcust810114514@yahoo.co.jp>
2022-10-11 00:29:13 +0900

    ロギングに開始時間を追加

--- logger.py
+++ logger.py
@@ -42,16 +42,16 @@ class Logger:
         f.flush()
     def main_loop(self):
         today = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-        #start_time = today + datetime.timedelta(hours=9, minutes=30)
+        start_time = today + datetime.timedelta(hours=9, minutes=30)
         limit_time = today + datetime.timedelta(hours=24)
         path = Path(config.log_directory()) / f"log_{today.date().isoformat()}.txt"
         with open(path, "a") as f:
             schedule.every(8).minutes.until(limit_time).do(self.log_once, f=f)
-            #schedule.every(30).seconds.until(limit_time).do(self.log_once, f=f)
             try:
-                while datetime.datetime.now() <= limit_time:
-                    schedule.run_pending()
-                    time.sleep(30)
+                while (now := datetime.datetime.now()) <= limit_time:
+                    if start_time <= now:
+                        schedule.run_pending()
+                        time.sleep(30)
             finally:
                 schedule.clear()
     @staticmethod
