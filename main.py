b180450793f71a319fc6b58345c6c9e65925106f
monadnadnad <hmcust810114514@yahoo.co.jp>
2022-11-03 12:19:01 +0900

    plotとデータ構築を分割

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ def get_buffer() -> BytesIO:
     with BytesIO() as buf:
         yield buf
 
+def plot_datetime(xs: list[datetime.datetime], ys: list[float], buffer: BytesIO):
+    # open
+    fig, ax = plt.subplots(figsize=(8,4))
+    ax.plot(xs, ys)
+    ax.minorticks_on()
+    ax.grid(alpha=0.8)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    fig.savefig(buffer, format="png")
+    buffer.seek(0)
+    # free
+    plt.close(fig)
+def plot_oneday(xs: list[datetime.timedelta], ys: list[float], buffer: BytesIO):
+    fake = datetime.datetime(2022,1,1)
+    xs = [fake + t for t in xs]
+    plot_datetime(xs, ys, buffer)
+
 class LogUsecase:
     def __init__(self):
         self.logs: dict[datetime.date, Log] = {}
@@ -35,49 +51,29 @@ class LogUsecase:
     # singleton
     def __call__(self):
         return self
-    def _plot_xdate(self, xs: list[datetime.datetime], ys: list[float], buffer: BytesIO):
-        fig, ax = plt.subplots(figsize=(8,4))
-        ax.plot(xs, ys)
-        ax.minorticks_on()
-        ax.grid(alpha=0.8)
-        ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-        fig.savefig(buffer, format="png")
-        buffer.seek(0)
-        # free
-        plt.close(fig)
-    def _plot_oneday(self, xs: list[datetime.timedelta], ys: list[float], buffer: BytesIO):
-        fake = datetime.datetime(2022,1,1)
-        xs = [fake + t for t in xs]
-        self._plot_xdate(xs, ys, buffer)
-    def plot_headcounts_of_date(self, _date: datetime.date, buffer: BytesIO):
-        log = self.logs[_date]
+    def get_headcounts_of_date(self, date: datetime.date) -> dict[datetime.datetime, float]:
+        log = self.logs[date]
         a = LogAnalyzer()
         times = log.get_times()
         headcounts = a.get_headcounts(log)
-        self._plot_xdate(times, headcounts, buffer)
-    def plot_headcounts_average(self, buffer: BytesIO):
+        return dict(zip(times, headcounts))
+    def get_headcounts_average(self) -> dict[datetime.timedelta, float]:
         a = LogAnalyzer()
         acc = OnedayAccumulator()
         for log in self.logs.values():
             times = log.get_times()
             headcounts = a.get_headcounts(log)
             acc.append(times, headcounts)
-        res = acc.result()
-        xs = list(res.keys())
-        avgs = list(map(fmean, res.values()))
-        self._plot_oneday(xs, avgs, buffer)
-    def plot_headcounts_median(self, buffer: BytesIO):
+        return dict((k, fmean(v)) for k, v in acc.result().items())
+    def get_headcounts_median(self):
         a = LogAnalyzer()
         acc = OnedayAccumulator()
         for log in self.logs.values():
             times = log.get_times()
             headcounts = a.get_headcounts(log)
             acc.append(times, headcounts)
-        res = acc.result()
-        xs = list(res.keys())
-        medians = list(map(median_high, res.values()))
-        self._plot_oneday(xs, medians, buffer)
-    def plot_headcounts_average_day_of_week(self, weekday: int, buffer: BytesIO):
+        return dict((k, median_high(v)) for k, v in acc.result().items())
+    def get_headcounts_average_weekday(self, weekday: int) -> dict[datetime.timedelta, float]:
         a = LogAnalyzer()
         acc = OnedayAccumulator()
         for _date, log in self.logs.items():
@@ -86,11 +82,8 @@ class LogUsecase:
             times = log.get_times()
             headcounts = a.get_headcounts(log)
             acc.append(times, headcounts)
-        res = acc.result()
-        xs = list(res.keys())
-        ys = list(map(fmean, res.values()))
-        self._plot_oneday(xs, ys, buffer)
-    def plot_playtime(self, id: str, buffer: BytesIO):
+        return dict((k, fmean(v)) for k, v in acc.result().items())
+    def get_playtime(self, id: str) -> dict[datetime.timedelta, float]:
         p = Player("fake", id)
         a = LogAnalyzer()
         acc = OnedayAccumulator()
@@ -98,10 +91,7 @@ class LogUsecase:
             times = log.get_times()
             piv = a.get_player_in_venue(log, p)
             acc.append(times, list(map(float, piv)))
-        res = acc.result()
-        xs = list(res.keys())
-        counts = list(map(sum, res.values()))
-        self._plot_oneday(xs, counts, buffer)
+        return dict((k, sum(v)) for k, v in acc.result().items())
     def get_all_playdate(self, id: str) -> list[datetime.date]:
         p = Player("fake", id)
         ret = []
@@ -135,7 +125,8 @@ async def player_time(
         log_usecase: LogUsecase = Depends(get_log_usecase),
         buffer = Depends(get_buffer)
     ):
-    log_usecase.plot_playtime(id, buffer)
+    playtime = log_usecase.get_playtime(id)
+    plot_oneday(playtime.keys(), playtime.values(), buffer)
     return StreamingResponse(buffer, media_type="image/png")
 
 @app.get("/headcounts/average")
@@ -143,7 +134,8 @@ async def headcounts_average(
         log_usecase: LogUsecase = Depends(get_log_usecase),
         buffer = Depends(get_buffer)
     ):
-    log_usecase.plot_headcounts_average(buffer)
+    data = log_usecase.get_headcounts_average()
+    plot_oneday(data.keys(), data.values(), buffer)
     return StreamingResponse(buffer, media_type="image/png")
 
 @app.get("/headcounts/average/{weekday}")
@@ -152,7 +144,8 @@ async def headcounts_average_of_weekday(
         log_usecase: LogUsecase = Depends(get_log_usecase),
         buffer = Depends(get_buffer)
     ):
-    log_usecase.plot_headcounts_average_day_of_week(weekday, buffer)
+    data = log_usecase.get_headcounts_average_weekday(weekday)
+    plot_oneday(data.keys(), data.values(), buffer)
     return StreamingResponse(buffer, media_type="image/png")
 
 @app.get("/headcounts/median")
@@ -160,14 +153,16 @@ async def headcounts_median(
         log_usecase: LogUsecase = Depends(get_log_usecase),
         buffer = Depends(get_buffer)
     ):
-    log_usecase.plot_headcounts_median(buffer)
+    data = log_usecase.get_headcounts_median()
+    plot_oneday(data.keys(), data.values(), buffer)
     return StreamingResponse(buffer, media_type="image/png")
 
-@app.get("/headcounts/{_date}")
+@app.get("/headcounts/{date}")
 async def headcounts_of_date(
-        _date: datetime.date,
+        date: datetime.date,
         log_usecase: LogUsecase = Depends(get_log_usecase),
         buffer = Depends(get_buffer)
     ):
-    log_usecase.plot_headcounts_of_date(_date, buffer)
+    data = log_usecase.get_headcounts_of_date(date)
+    plot_datetime(data.keys(), data.values(), buffer)
     return StreamingResponse(buffer, media_type="image/png")
